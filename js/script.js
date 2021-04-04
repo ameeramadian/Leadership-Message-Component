f9e91513a48ae9b9292adc60b5f159709068eee9
Ameera Madian <ameera.madian@itworx.com>
2021-04-04 16:31:41 +0200

    first layout

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
 
 const seeMoreButton = document.querySelector("#see-more-btn");
 const seeLessButton = document.querySelector("#see-less-btn");
+const longParag = document.querySelector("#collapseOne")
+
+seeLessButton.style.display = "none"
 
 seeMoreButton.addEventListener('click',event => {
-        seeMoreButton.style.display = "none"
-        seeLessButton.style.display = "inline"
+    seeMoreButton.style.display = "none"
+    seeLessButton.style.display = "inline"
+    longParag.classList.toggle("invisible");
+    longParag.classList.toggle("d-none");
 });
 
 seeLessButton.addEventListener('click',event => {
-    seeLessButton.style.display = "none"
     seeMoreButton.style.display = "inline"
+    seeLessButton.style.display = "none"
+    longParag.classList.toggle("invisible");
+    longParag.classList.toggle("d-none");
 });
 
