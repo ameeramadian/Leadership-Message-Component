dd7d001580c995dfe14c79d9a54acbfd3fe61e2f
Ameera Madian <ameera.madian@itworx.com>
2021-04-05 11:25:21 +0200

    code reorgaanization

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ const secondLayoutBtn = document.querySelector("#btnSecondLayout");
 const thirdLayoutBtn = document.querySelector("#btnThirdLayout");
 
 // elements for re-ordering
-const nameTitleDiv = document.querySelector("#titleDiv");
 const leftColumn = document.querySelector("#leftColumn");
 const rightColumn = document.querySelector("#rightColumn");
 const leaderImage = document.querySelector("#leaderImage");
@@ -12,75 +11,14 @@ const leaderImage = document.querySelector("#leaderImage");
 //elemnts for re-styling 
 const messageDiv = document.querySelector("#messageDiv");
 const titleDiv = document.querySelector("#titleDiv");
+const titleText = document.querySelector("#titleText")
 
 // elements for showing and hiding 
 const seeMoreButton = document.querySelector("#see-more-btn");
 const seeLessButton = document.querySelector("#see-less-btn");
 const longParag = document.querySelector("#collapseOne");
 
-
-
-////////////////////////////////////////////////////////////////First Layout ////////////////////////////////////////////////////////////////
-
-// styles
-const backgroundDarkerBlue = "#0c6396";
-const backgroundLighterBlue = "#008dc4";
-
-function styleFirstLayoutTextButtons(){
-    seeMoreButton.classList.toggle("text-white", true);
-    seeMoreButton.style.textDecoration = "underline";
-    seeMoreButton.style.textDecorationColor = "white";
-
-    seeLessButton.classList.toggle("text-white", true);
-    seeLessButton.style.textDecoration = "underline";
-    seeLessButton.style.textDecorationColor = "white";
-}
-
-function removeTextFromTopOfImage(){
-    leftColumn.classList.remove("position-relative");
-    titleDiv.classList.remove("position-absolute");
-}
-
-function styleHorizontalLayout(){
-    if(rightColumn.classList.contains("col-12")){
-        rightColumn.classList.add("col-8")
-        rightColumn.classList.remove("col-12")
-        rightColumn.classList.remove("col-md-8")
-    }
-    if(leftColumn.classList.contains("col-12")){
-        leftColumn.classList.add("col-4");
-        leftColumn.classList.remove("col-12");
-        leftColumn.classList.remove("col-md-4");
-        leftColumn.classList.remove("m-0");
-    }
-}
-
-function setFirstButtonActive(){
-    firstLayoutBtn.classList.add("active");
-    secondLayoutBtn.classList.remove("active");
-    thirdLayoutBtn.classList.remove("active");
-}
-
-function styleFirstLayout(){
-    messageDiv.style.background = backgroundLighterBlue;
-    titleDiv.style.background = backgroundDarkerBlue;
-    messageDiv.classList.toggle("text-white",true);
-    titleDiv.style.opacity = "1";
-    seeMoreButton.classList.remove("my-4")
-    seeLessButton.classList.remove("my-4")
-    styleHorizontalLayout()
-    removeTextFromTopOfImage()
-    styleFirstLayoutTextButtons();
-}
-
-setFirstButtonActive();
-styleFirstLayout();
-
-firstLayoutBtn.addEventListener('click',event => {
-   rightColumn.appendChild(nameTitleDiv);
-   setFirstButtonActive();
-   styleFirstLayout();
-});
+/////// expaand and collapse text 
 
 seeLessButton.style.display = "none"
 
@@ -98,38 +36,112 @@ seeLessButton.addEventListener('click',event => {
     longParag.classList.toggle("d-none");
 });
 
+//////////////////////////////////////////////////////////////// Layout  Stylings ////////////////////////////////////////////////////////////////
+
+function setLayoutButtonActive(activeLayout){
+    switch (activeLayout) {
+         case 2 : { 
+            firstLayoutBtn.classList.remove("active");
+            secondLayoutBtn.classList.add("active");
+            thirdLayoutBtn.classList.remove("active");
+            break;
+         }
+         case 3 : {
+            firstLayoutBtn.classList.remove("active");
+            secondLayoutBtn.classList.remove("active");
+            thirdLayoutBtn.classList.add("active");
+            break;
+         }
+         default: {
+            firstLayoutBtn.classList.add("active");
+            secondLayoutBtn.classList.remove("active");
+            thirdLayoutBtn.classList.remove("active");
+            break;
+         }
+    }
+}
+
+function styleTextButtons(buttonTextColor, decorationColor){
+    if (buttonTextColor  == "white" ){
+
+        seeMoreButton.classList.toggle("text-white", true);
+        seeMoreButton.style.textDecoration = "underline";
+        seeMoreButton.style.textDecorationColor = decorationColor;
+    
+        seeLessButton.classList.toggle("text-white", true);
+        seeLessButton.style.textDecoration = "underline";
+        seeLessButton.style.textDecorationColor = decorationColor;
+
+    } else {
+
+        seeMoreButton.classList.toggle("text-white", false);
+        seeMoreButton.style.color = buttonTextColor;
+        seeMoreButton.style.textDecoration = "underline";
+        seeMoreButton.style.textDecorationColor = decorationColor;
+
+        seeLessButton.classList.toggle("text-white", false);
+        seeLessButton.style.color = buttonTextColor;
+        seeLessButton.style.textDecoration = "underline";
+        seeLessButton.style.textDecorationColor = decorationColor;
+
+    }
+}
+
+////////////////////////////////////////////////////////////////First Layout ////////////////////////////////////////////////////////////////
+
+// styles
+const backgroundDarkerBlue = "#0c6396";
+const backgroundLighterBlue = "#008dc4";
+
+function styleHorizontalLayout(){
+    if(rightColumn.classList.contains("col-12")){
+        rightColumn.classList.add("col-8")
+        rightColumn.classList.remove("col-12")
+        rightColumn.classList.remove("col-md-8")
+    }
+    if(leftColumn.classList.contains("col-12")){
+        leftColumn.classList.add("col-4");
+        leftColumn.classList.remove("col-12");
+        leftColumn.classList.remove("col-md-4");
+        leftColumn.classList.remove("m-0");
+    }
+}
+
+function setBackgroundBlue(){
+    messageDiv.style.background = backgroundLighterBlue;
+    titleDiv.style.background = backgroundDarkerBlue;
+    titleDiv.style.opacity = "1";
+}
+
+function removeTitleFromTopOfImage(){
+    leftColumn.classList.remove("position-relative");
+    titleDiv.classList.remove("position-absolute");
+}
+
+function styleFirstLayout(){
+    messageDiv.classList.toggle("text-white",true);
+    seeMoreButton.classList.remove("my-4")
+    seeLessButton.classList.remove("my-4")
+    setBackgroundBlue()
+    styleHorizontalLayout()
+    removeTitleFromTopOfImage()
+    styleTextButtons("white", "white");
+}
+
+setLayoutButtonActive(1);
+styleFirstLayout();
+
+firstLayoutBtn.addEventListener('click',event => {
+   rightColumn.appendChild(titleDiv);
+   setLayoutButtonActive(1);
+   styleFirstLayout();
+});
 
 //////////////////////////////////////////////////////////////// second Layout ////////////////////////////////////////////////////////////////
 
-const backgroundWhite = "#fff";
 const backgroundLessOpaqueDarkeBlue = "#0A2554";
 const textDarkBlue = "#0A2554";
 const textDecoartionGreen = "#42C3AF";
-
-function setSecondButtonActive(){
-    firstLayoutBtn.classList.remove("active");
-    secondLayoutBtn.classList.add("active");
-    thirdLayoutBtn.classList.remove("active");
-}
-
-function styleSecondLayoutText(){
-    messageDiv.classList.toggle("text-white", false);
-    messageDiv.style.color = textDarkBlue;
-}
-
-function styleSecondLayoutTextButtons(){
-    seeMoreButton.classList.toggle("text-white", false);
-    seeMoreButton.style.color = textDarkBlue;
-    seeMoreButton.style.textDecoration = "underline";
-    seeMoreButton.style.textDecorationColor = "textDecoartionGreen";
-
-    seeLessButton.classList.toggle("text-white", false);
-
-    seeLessButton.style.color = textDarkBlue;
-    seeLessButton.style.textDecoration = "underline";
-    seeLessButton.style.textDecorationColor = "textDecoartionGreen";
-
-}
 
 function styleVerticalLayout(){
     if(rightColumn.classList.contains("col-8")){
@@ -145,48 +157,48 @@ function styleVerticalLayout(){
     }
 }
 
-function makeTextOnTopOfImage(){
+function addTitleOnTopOfImage(){
     leftColumn.classList.add("position-relative");
     titleDiv.classList.add("position-absolute");
     titleDiv.style.bottom = "26px";
     titleDiv.style.left = "1px;"
 }
 
-function styleSecondLayout(){     
-    messageDiv.style.background = backgroundWhite;
+function setBackgroundWhite(){
+    messageDiv.style.background = "white";
     titleDiv.style.background = backgroundLessOpaqueDarkeBlue;
     titleDiv.style.opacity = "0.7";
+}
+
+function setTextDark(){
+    messageDiv.classList.toggle("text-white", false);
+    messageDiv.style.color = textDarkBlue;
+}
+
+function styleSecondLayout(){     
     seeMoreButton.classList.add("my-4")
     seeLessButton.classList.add("my-4")
-    seeMoreButton.style.textDecorationColor = textDecoartionGreen;
     styleVerticalLayout()
-    makeTextOnTopOfImage()
-    styleSecondLayoutText();
-    styleSecondLayoutTextButtons();
+    setBackgroundWhite()
+    addTitleOnTopOfImage()
+    setTextDark();
+    styleTextButtons(textDarkBlue, textDecoartionGreen);
 }
 
 function repositionTitleDiv(){
-    nameTitleDiv.remove();
-    leftColumn.appendChild(nameTitleDiv);
+    titleDiv.remove();
+    leftColumn.appendChild(titleDiv);
 }
 
 secondLayoutBtn.addEventListener('click',event => {
     repositionTitleDiv(); 
-    setSecondButtonActive();
+    setLayoutButtonActive(2);
     styleSecondLayout();
     
     
  });
 
  //////////////////////////////////////////////////////////////// third Layout ////////////////////////////////////////////////////////////////
-
-const titleText = document.querySelector("#titleText")
-
- function setthirdButtonActive(){
-    firstLayoutBtn.classList.remove("active");
-    secondLayoutBtn.classList.remove("active");
-    thirdLayoutBtn.classList.add("active");
-}
 
 function placeImageinTitleDiv(){
     leftColumn.remove();
@@ -198,7 +210,7 @@ function placeImageinTitleDiv(){
 }
 
  thirdLayoutBtn.addEventListener('click',event => {
-    setthirdButtonActive();
+    setLayoutButtonActive(3);
     // placeImageinTitleDiv();
  });
 
